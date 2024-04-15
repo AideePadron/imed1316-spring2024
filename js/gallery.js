edb6dd09624dbe468927f4cd78edde34267ff760
AideePadron <156817207+AideePadron@users.noreply.github.com>
2024-04-15 12:05:10 -0500

    added tags

---
+++ js/gallery.js
@@ -0,0 +1,12 @@
+// A $( document ).ready() block.
+$(document).ready(function () {
+    //sconsole.log("ready!");
+
+
+
+    $("#blinn-gallery").nanogallery2({
+        ...
+        thumbnailHeight: 200,
+        thumbnailWidth: 200
+    });
+});
