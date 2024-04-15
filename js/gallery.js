ce82706596e27af5c61f05d4a0a42fd34f64e0f5
AideePadron <156817207+AideePadron@users.noreply.github.com>
2024-04-15 12:18:18 -0500

    added styles

--- js/gallery.js
+++ js/gallery.js
@@ -5,8 +5,18 @@ $(document).ready(function () {
 
 
     $("#blinn-gallery").nanogallery2({
-        ...
-        thumbnailHeight: 200,
-        thumbnailWidth: 200
+        thumbnailWidh: "300",
+        thumbnailHeight: "auto",
+        thumbnailBorderVertical: 0,
+        thumbnailBorderHorizontal: 0,
+        thumbnailLabel: {
+            position: "overImageOnBottom",
+            display: false
+        },
+        thumbnailHoverEffect2: "imageScaleIn80|imageSepiaOff|labelAppear75",
+        thumbnailGutterWith: 0,
+        thumbnailGutterHeight: 0,
+        thumbnailAllignment: "center",
+        thumbnailOpenImage: true
     });
 });
